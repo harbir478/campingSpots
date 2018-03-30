cca4f5bb3229391234a9c282ae21fbcfb2544271
Harbir Singh <extremeposterz@gmail.com>
2018-03-30 08:50:39 +0000

    Database env

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect(process.env.DATABASEURL);
+mongoose.connect("mongodb://harbir1:harbir@ds241658.mlab.com:41658/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
